4093ed0f632a5b9646a930905ad8e64b42446541
Harsha Vardhan Jagadeesan <vardhanharshaj@gmail.com>
2018-05-05 18:56:42 +0530

    Added variable that points to home folder
    Have added the ruby variable that points to the home folder of the user
    and creates a text file there, if it doesn't already exist, and adds
    content to it.

--- hello.rb
+++ hello.rb
@@ -1,5 +1,5 @@
-#Chef recipe that creates a file and adds certain content to it
+#Chef recipe that creates a file in the home folder of the user and adds certain content to it
 
-file 'hello.txt' do
+file "#{ENV['HOME']}/hello.txt" do
 	content "First Chef Recipe!"
 end
